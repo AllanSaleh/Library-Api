b21cf6bac7e7fd5dacba856ce1c728a039db91fc
Allan Ahmed <allan@allans-mbp-2.home>
2025-11-26 04:06:51 -0500

    test driven development

--- config.py
+++ config.py
@@ -5,7 +5,11 @@ class DevelopmentConfig:
   CACHE_DEFAULT_TIMEOUT = 300
 
 class TestingConfig:
-  pass
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
+  DEBUG = True
+  CACHE_TYPE = "SimpleCache"
+  CACHE_DEFAULT_TIMEOUT = 300
+  TESTING = True
 
 class ProductionConfig:
   pass
